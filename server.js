07986aa234a629011de818c988039bc19fcf2c75
Muosvr <lujason2015@gmail.com>
2019-05-16 11:22:39 -0400

    Intro to Express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/', express.static("public"));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/api', function (req, res) {
+  const userName = req.query.username;
+  const userId = req.query.id;
+  const message = req.query.message;
+  console.log(userName);
+  const reply = `${userName} with id of ${userId} is saying ${message}`
+  res.send(reply);
+})
+
+app.get("/showprofile/:username", function (req, res) {
+  const user = req.params.username;
+  console.log(user);
+
+  res.send("show profile working");
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
