f94657ac24e1d7c21959fda5e1523704933612c4
Muosvr <lujason2015@gmail.com>
2019-05-16 20:52:19 -0400

    May 16th Thursday lecture

--- public/script.js
+++ public/script.js
@@ -1,1 +1,8 @@
-console.log("scripts working");
+function getUserInfo() {
+  const input = document.getElementById("userName").value;
+  const url = "/showprofile/" + input;
+  axios.get(url)
+    .then(response => {
+      document.getElementById("userInfo").innerHTML = response.data;
+    })
+}
